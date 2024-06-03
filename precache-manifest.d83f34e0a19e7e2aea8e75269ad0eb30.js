d7515af0c01f93d969f9d194f60dd08db94976fd
OlegYar <117899632+OlegYar228@users.noreply.github.com>
2024-06-03 14:32:52 +0300

    Add files via upload

---
+++ precache-manifest.d83f34e0a19e7e2aea8e75269ad0eb30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1689bdada4fd24f982a47ba4d18490a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a13b523c732d2082d022",
+    "url": "/static/css/main.588b62a9.chunk.css"
+  },
+  {
+    "revision": "e4227eed6cb6b4c6be8b",
+    "url": "/static/js/2.4790b6cb.chunk.js"
+  },
+  {
+    "revision": "a13b523c732d2082d022",
+    "url": "/static/js/main.dcf84137.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
